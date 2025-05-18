0f83ca141569d2244766700d836ab177e6778d7a
Terence Chau <34471214+terencechau@users.noreply.github.com>
2025-05-18 09:44:07 -0400

    plots

--- event-study/plot_data.py
+++ event-study/plot_data.py
@@ -6,8 +6,18 @@ import plotly.express as px
 panel = pd.read_csv("output/panel_data.csv")
 
 # Plot group means for ever vs never treated
+# Note: sns can compute the group averages automatically,
+# but it can't overlay dots unless the means are pre-computed
+group_means = (
+    panel
+    .groupby(by=['time', 'ever_treated'])['y']
+    .mean()
+    .reset_index()
+)
+
 trend_plot, ax = plt.subplots(figsize=(8, 5))
 sns.lineplot(data=panel, x="time", y="y", hue="ever_treated", ax=ax)
+sns.scatterplot(data=group_means, x="time", y="y", hue="ever_treated", ax=ax)
 
 ax.set_title("Average Outcome Over Time by Treatment Group")
 ax.set_xlabel("Time")
@@ -19,13 +29,6 @@ trend_plot.show()
 trend_plot.savefig("output/treatment_trends.png", dpi=300)
 
 # Using plotly (no CIs, unless computed by hand)
-group_means = (
-    panel
-    .groupby(by=['time', 'ever_treated'])['y']
-    .mean()
-    .reset_index()
-)
-
 trend_plotly = px.line(group_means, x='time', y='y', color='ever_treated',
               title="Outcome Over Time by Treatment Group")
 trend_plotly.show()
